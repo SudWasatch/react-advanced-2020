a25af5e6d4feace7a585f08118f1a84915a8c27a
SudWasatch <126173539+SudWasatch@users.noreply.github.com>
2023-02-26 08:08:52 +0000

    todays commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from 'react'
-import Setup from './tutorial/6-useReducer/setup'
+import React from "react";
+import Setup from "./tutorial/12-memo-useMemo-useCallback/setup";
 function App() {
   return (
-    <div className='container'>
-      <Setup/>
+    <div className="container">
+      <Setup />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
